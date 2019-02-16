3eda1d8796b823b94269ba9e3a532a26123a1e35
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-02-16 12:19:06 +0200

    Added file query to page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,51 @@
 import React from "react";
 import Layout from "../components/layout";
-import { Link } from "gatsby";
+import { Link, graphql, StaticQuery } from "gatsby";
+
+const getImageData = graphql`
+  {
+    allFile {
+      totalCount
+      edges {
+        node {
+          relativePath
+          absolutePath
+          extension
+          size
+        }
+      }
+    }
+  }
+`;
 
 export default () => (
   <Layout>
     <h1>About</h1>
+
+    <StaticQuery
+      query={getImageData}
+      render={data => (
+        <table>
+          <thead>
+            <tr>
+              <th>Relative Path</th>
+              <th>Extension</th>
+              <th>Size</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.extension}</td>
+                <td>{node.size}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    />
+
     <Link to="/page-2">Page 2</Link>
   </Layout>
 );
