5461dad3f0c7b09b8c08f02ca0dd7dff238395d0
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-02-16 11:30:11 +0200

    Added a new page, modified main graphql query.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,25 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import Header from "./header";
+import "./layout.css";
+
+const getSiteMetaData = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        author
+        createdAt
+      }
+    }
+  }
+`;
 
 const Layout = ({ children }) => (
   <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+    query={getSiteMetaData}
     render={data => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
@@ -24,23 +28,23 @@ const Layout = ({ children }) => (
             margin: `0 auto`,
             maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
+            paddingTop: 0
           }}
         >
           <main>{children}</main>
           <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            © {new Date().getFullYear()}. Built by{" "}
+            {data.site.siteMetadata.author}. Created in{" "}
+            {data.site.siteMetadata.createdAt}
           </footer>
         </div>
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+  children: PropTypes.node.isRequired
+};
 
-export default Layout
+export default Layout;
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/layout";
+import { Link } from "gatsby";
+
+export default () => (
+  <Layout>
+    <h1>About</h1>
+    <Link to="/page-2">Page 2</Link>
+  </Layout>
+);
