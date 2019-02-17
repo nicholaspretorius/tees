0c47c4557d2d9cd3666ae4142ca61d2303ce7331
Nicholas Pretorius <nicholaspretorius@gmail.com>
2019-02-17 11:30:00 +0200

    Added contenful and dotenv.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
+const dotenv = require("dotenv");
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
+
 module.exports = {
   siteMetadata: {
     title: `Tees`,
@@ -14,7 +20,17 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
-    `gatsby-transformer-remark`,
+    //`gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`
+          }
+        ]
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -34,6 +50,13 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_API_DELIVERY
       }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/blog/three.md
+++ src/pages/blog/three.md
@@ -3,4 +3,6 @@ title: "Third Post."
 date: "2019-02-14"
 ---
 
-This is my third post!
+This is my third post with an image!
+
+![Beautiful Beach](images/beach.jpg)
